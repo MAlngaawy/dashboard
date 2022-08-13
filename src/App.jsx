5ff7cc608faa5fc235a0696768562810a390fc68
MAlngaawy <m.alngaawy@gmail.com>
2022-08-13 18:22:48 +0200

    create contextAPI and use it in sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider';
 
 import './App.css'
 
 const App = () => {
-  const activeMenu = true
+
+  const {activeMenu} = useStateContext()
+
   return <div>
     <BrowserRouter>
       <div className='relative flex dark:bg-main-dark-bg'>
