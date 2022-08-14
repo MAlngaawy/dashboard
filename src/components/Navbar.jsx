f5cdfc9b12496c400b8a7c5c31025664266e3678
MAlngaawy <m.alngaawy@gmail.com>
2022-08-15 01:21:27 +0200

    fix build issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
     window.addEventListener('resize', handleResize)
     handleResize()
     return () => window.removeEventListener('resize', handleResize)
-  },[]);
+  },[setScreenSize]);
 
   useEffect(() => {
     if(screenSize <= 900) {
@@ -34,7 +34,7 @@ const Navbar = () => {
     } else {
       setActiveMenu(true)
     }
-  },[screenSize])
+  },[screenSize,setActiveMenu])
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
