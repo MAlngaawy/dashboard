4113a3f4584ec48ee3d43b0e947df9b868da429d
MAlngaawy <m.alngaawy@gmail.com>
2022-08-14 23:05:37 +0200

    create the click functionality in the context provider

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -5,17 +5,22 @@ const StateContext = createContext();
 const initialState = {
   chat: false,
   cart: false,
-  useProfile: false,
+  userProfile: false,
   notification: false
 }
 
 export const ContextProvider = ({children}) => {
 
   const [activeMenu, setActiveMenu] = useState(true)
+  const [isClicked, setIsClicked] = useState(initialState)
+
+  const handleClick = (clicked) => {
+    setIsClicked({...initialState, [clicked] : true})
+  }
 
   return (
     <StateContext.Provider
-      value={{activeMenu, setActiveMenu}}
+      value={{activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick}}
     >
       {children}
     </StateContext.Provider>
