6fe719cf1c7db459010e3cc585389437207bbe95
MAlngaawy <m.alngaawy@gmail.com>
2022-08-22 11:10:22 +0200

    install prettier and use it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,56 +1,102 @@
-import React, {useEffect} from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
-import { FiShoppingCart } from 'react-icons/fi';
-import { BsChatLeft } from 'react-icons/bs';
-import { RiNotificationLine } from 'react-icons/ri';
-import { MdKeyboardArrowDown } from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import avatar from '../data/avatar.jpg'
-import {Cart, Chat, Notification, UserProfile} from '.'
-import { useStateContext } from '../contexts/ContextProvider';
+import React, { useEffect } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotificationLine } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import avatar from "../data/avatar.jpg";
+import { Cart, Chat, Notification, UserProfile } from ".";
+import { useStateContext } from "../contexts/ContextProvider";
 
-const NavButton = ({title, customFunc, icon, color, dotColor}) => (
-  <TooltipComponent content={title} position='BottomCenter'>
-    <button type='button' onClick={customFunc} style={{color}} className='relative text-xl rounded-full p-3 hover:bg-light-gray' >
-      <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-1' />
-        {icon}
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      type="button"
+      onClick={customFunc}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-1"
+      />
+      {icon}
     </button>
   </TooltipComponent>
-)
+);
 
 const Navbar = () => {
-  const { setActiveMenu, isClicked, handleClick, screenSize, setScreenSize} = useStateContext()
+  const { setActiveMenu, isClicked, handleClick, screenSize, setScreenSize } =
+    useStateContext();
 
   useEffect(() => {
-    const handleResize = () => {console.log("Resized"); setScreenSize(window.innerWidth)}
-    window.addEventListener('resize', handleResize)
-    handleResize()
-    return () => window.removeEventListener('resize', handleResize)
-  },[setScreenSize]);
+    const handleResize = () => {
+      console.log("Resized");
+      setScreenSize(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, [setScreenSize]);
 
   useEffect(() => {
-    if(screenSize <= 900) {
-      setActiveMenu(false)
+    if (screenSize <= 900) {
+      setActiveMenu(false);
     } else {
-      setActiveMenu(true)
+      setActiveMenu(true);
     }
-  },[screenSize,setActiveMenu])
+  }, [screenSize, setActiveMenu]);
 
   return (
-    <div className='flex justify-between p-2 md:mx-6 relative'>
-      <NavButton title='Menu' customFunc={() => setActiveMenu((prev) => !prev)} icon={<AiOutlineMenu />} />
+    <div className="flex justify-between p-2 md:mx-6 relative">
+      <NavButton
+        title="Menu"
+        customFunc={() => setActiveMenu((prev) => !prev)}
+        icon={<AiOutlineMenu />}
+      />
       <div className="flex">
-        <NavButton title='Cart' customFunc={() => {handleClick('cart')}} color='blue' icon={<FiShoppingCart />} />
-        <NavButton title='Chat' dotColor='#03c9d7' customFunc={() => {handleClick('chat')}} color='blue' icon={<BsChatLeft />} />
-        <NavButton title='Notifications' dotColor='#03c9d7' customFunc={() => {handleClick('notification')}} color='blue' icon={<RiNotificationLine />} />
-        <TooltipComponent content='profile' position='BottomCenter' >
-          <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg' onClick={() => {handleClick('userProfile')}}>
-            <img className='rounded-full w-8 h-8' src={avatar} alt='avatar' />
+        <NavButton
+          title="Cart"
+          customFunc={() => {
+            handleClick("cart");
+          }}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+        <NavButton
+          title="Chat"
+          dotColor="#03c9d7"
+          customFunc={() => {
+            handleClick("chat");
+          }}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          dotColor="#03c9d7"
+          customFunc={() => {
+            handleClick("notification");
+          }}
+          color="blue"
+          icon={<RiNotificationLine />}
+        />
+        <TooltipComponent content="profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => {
+              handleClick("userProfile");
+            }}
+          >
+            <img className="rounded-full w-8 h-8" src={avatar} alt="avatar" />
             <p>
-              <span className='text-gray-400 text-14'>Hi, </span>
-              <span className='text-gray-400 text-14 font-bold ml-1'>Mohammed </span>
+              <span className="text-gray-400 text-14">Hi, </span>
+              <span className="text-gray-400 text-14 font-bold ml-1">
+                Mohammed{" "}
+              </span>
             </p>
-            <MdKeyboardArrowDown className='text-gray-400 text-14' />
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
         {isClicked.cart && <Cart />}
@@ -59,7 +105,7 @@ const Navbar = () => {
         {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import App from './App.jsx'
-import { ContextProvider } from './contexts/ContextProvider';
+import App from "./App.jsx";
+import { ContextProvider } from "./contexts/ContextProvider";
 
 ReactDOM.render(
   <ContextProvider>
     <App />
-  </ContextProvider>
-  ,document.getElementById('root'))
+  </ContextProvider>,
+  document.getElementById("root")
+);
